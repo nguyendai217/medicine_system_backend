c4bd22b57c8ab95bc0bcbd7cb860847639cf937b
nguyendai217 <nguyendai2171998@gmail.com>
2023-08-08 20:25:30 +0700

    update route api order

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import productsRoutes from "./routes/products.js";
 import unitsRoutes from "./routes/units.js";
 import customersRoutes from "./routes/customers.js";
 import oldPriceRoutes from "./routes/oldPrice.js";
+import orderRoutes from "./routes/order.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import multer from "multer";
@@ -27,6 +28,7 @@ app.use("/api/products", productsRoutes);
 app.use("/api/units", unitsRoutes);
 app.use("/api/customers", customersRoutes);
 app.use("/api/old_price", oldPriceRoutes);
+app.use("/api/order", orderRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("MyApp connected, running on port", process.env.PORT);

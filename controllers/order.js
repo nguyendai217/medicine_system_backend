eac25e9ad99ec81c07b87bd90553e27c93d82f82
nguyendai217 <nguyendai2171998@gmail.com>
2023-08-10 22:35:04 +0700

    add api add order

--- controllers/order.js
+++ controllers/order.js
@@ -18,3 +18,28 @@ export const getOrderById = (req, res) => {
     });
 };
 
+export const addOrder = (req, res) => {
+    const q =
+        "INSERT INTO medicine.order(order_date, customer_id, total_price, note, total_product, profit, status, phone_customer) VALUES (?)";
+
+    const values = [
+        new Date(),
+        req.body.customer_id,
+        req.body.total_price,
+        req.body.note,
+        req.body.total_product,
+        req.body.profit,
+        1,
+        req.body.phone_customer
+    ];
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
+        console.log(data);
+        var insertId = data.insertId;
+        var result = {};
+        result['insertId'] = insertId;
+        result['message'] = "Thêm mới thành công.";
+        return res.status(201).json(result);
+    });
+};
--- routes/order.js
+++ routes/order.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
     getOrders,
     getOrderById,
+    addOrder
 } from "../controllers/order.js";
 
 const router = express.Router();
@@ -9,5 +10,5 @@ const router = express.Router();
 // all route api products
 router.get("/", getOrders);
 router.get("/:id", getOrderById);
-
+router.post("/", addOrder);
 export default router;
